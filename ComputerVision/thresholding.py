a91710ea087f0100bc0de36eae4b7d0e2e015c28
sharan376002 <sharang.developer@gmail.com>
2026-03-10 19:30:39 +0530

    intro_to_thresholding

---
+++ ComputerVision/thresholding.py
@@ -0,0 +1,35 @@
+import cv2
+
+img2 = cv2.imread('D:\ML-DL-learns\ComputerVision\images\sign.jpg')
+
+
+
+"""
+
+threshold is 
+Essentially, it’s about setting a cutoff point — known as the threshold — and anything above this point becomes white, 
+while anything below it turns black.
+
+For example, if you’re working on detecting text in a scanned document, 
+thresholding can help you isolate the text from the background by turning the text black and the background white.
+
+It’s a straightforward yet powerful tool, especially when you need to simplify an image for further analysis
+
+
+
+for example :
+     when thresholding is import is like there is a scanned document that background isnt uniform white and the text 
+     isnt perfect black so in that place wheere thresholding is realy helpful 
+
+It’s a straightforward yet powerful tool, especially when you need to simplify an image for further analysis
+
+
+
+"""
+
+
+# 1 .binnarry threshold 
+
+
+
+
